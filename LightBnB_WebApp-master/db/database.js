ecbf15db0616a7078d8f111eacc3bc681dd471df
Maxime <maxime.marechalmccoy@gmail.com>
2023-09-21 01:53:16 -0700

    add new property

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -225,11 +225,79 @@ const getAllProperties = function(options, limit = 10) {
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  // const propertyId = Object.keys(properties).length + 1;
-  // property.id = propertyId;
-  // properties[propertyId] = property;
-  // return Promise.resolve(property);
 
+  const {
+    owner_id,
+    title,
+    description = "",
+    thumbnail_photo_url,
+    cover_photo_url,
+    cost_per_night,
+    street,
+    city,
+    province,
+    post_code,
+    country,
+    parking_spaces,
+    number_of_bathrooms,
+    number_of_bedrooms
+  } = property;
+
+  // const queryParams = [...Object.values(property)];
+  const queryParams = [
+    Number(owner_id),
+    title,
+    description,
+    thumbnail_photo_url,
+    cover_photo_url,
+    Number(cost_per_night),
+    street,
+    city,
+    province,
+    post_code,
+    country,
+    Number(parking_spaces),
+    Number(number_of_bathrooms),
+    Number(number_of_bedrooms)
+  ];
+  let valuesToInsert = '';
+  queryParams.forEach((val, index) => {
+    console.log("🚀 ~ file: database.js:231 ~ valuesToInsert ~ index:", index);
+    if (index === queryParams.length - 1) {
+      return valuesToInsert += `$${index + 1}`;
+    }
+    return valuesToInsert += `$${index + 1},`;
+  });
+  console.log("🚀 ~ file: database.js:231 ~ addProperty ~ string:", valuesToInsert);
+  let queryString = `
+  INSERT INTO
+  properties (
+    owner_id,
+    title,
+    description,
+    thumbnail_photo_url,
+    cover_photo_url,
+    cost_per_night,
+    street,
+    city,
+    province,
+    post_code,
+    country,
+    parking_spaces,
+    number_of_bathrooms,
+    number_of_bedrooms
+  ) VALUES (
+  `;
+  queryString += valuesToInsert + ') RETURNING *;';
+
+  return pool.query(queryString, queryParams)
+    .then(res => {
+      return res.rows[0];
+    }
+    )
+    .catch(err => {
+      return console.log('error when adding property', err.message);
+    });
 };
 
 module.exports = {
